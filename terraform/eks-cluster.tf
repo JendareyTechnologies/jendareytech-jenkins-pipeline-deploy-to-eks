7d46ca69b422101db6972339a2d186a5a8e127eb
JendareyTechnologies <ak@jendareyconsulting.com>
2023-12-21 14:00:39 -0600

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -15,7 +15,7 @@ module "eks" {
     }
 
     eks_managed_node_groups = {
-        prod = {
+    prod-eks-votingapp-node = {
             min_size = 1
             max_size = 3
             desired_size = 2
