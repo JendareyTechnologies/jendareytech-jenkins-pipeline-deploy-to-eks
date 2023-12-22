5255c50044a37559e5aec25b2e61c725c55ef3b4
JendareyTechnologies <ak@jendareyconsulting.com>
2023-12-21 22:55:22 -0600

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,24 @@
-variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+variable "availability_zones" {
+  description = "List of availability zones for subnets"
+  type        = list(string)
 }
 
 variable "private_subnet_cidr_blocks" {
   description = "CIDR blocks for private subnets"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  type        = list(string)
 }
 
 variable "public_subnet_cidr_blocks" {
   description = "CIDR blocks for public subnets"
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  type        = list(string)
 }
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+}
+
+variable "workstation_external_cidr" {
+  description = "CIDR block for the external workstation"
+}
+
+# Add any additional variables you may need
