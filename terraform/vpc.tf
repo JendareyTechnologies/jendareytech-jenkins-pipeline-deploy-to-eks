1bb703aecbcd95015d9e7f94119962cbeb8a214f
JendareyTechnologies <ak@jendareyconsulting.com>
2023-12-22 19:02:11 -0600

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,81 +1,29 @@
-# Define VPC
-resource "aws_vpc" "votingapp" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+module "jendarey_vpc_eks" {
+  source          = "terraform-aws-modules/vpc/aws"
+  name            = "jendarey_vpc_eks"
+  cidr            = var.vpc_cidr_block
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
+  azs             = data.aws_availability_zones.azs.names
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "terraform-eks-votingapp"
+    "kubernetes.io/cluster/votingapp-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/votingapp-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/votingapp-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
   }
 }
 
-# Define private subnets
-resource "aws_subnet" "private_subnets" {
-  count = length(var.private_subnet_cidr_blocks)
-
-  vpc_id                  = aws_vpc.votingapp.id
-  cidr_block              = var.private_subnet_cidr_blocks[count.index]
-  availability_zone       = element(var.availability_zones, count.index)
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "private-subnet-${count.index + 1}"
-  }
-}
-
-# Define public subnets
-resource "aws_subnet" "public_subnets" {
-  count = length(var.public_subnet_cidr_blocks)
-
-  vpc_id                  = aws_vpc.votingapp.id
-  cidr_block              = var.public_subnet_cidr_blocks[count.index]
-  availability_zone       = element(var.availability_zones, count.index)
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-${count.index + 1}"
-  }
-}
-
-# Define security group rule for allowing workstation to communicate with the cluster API Server
-resource "aws_security_group_rule" "votingapp-cluster-ingress-workstation-https" {
-  cidr_blocks       = [var.workstation_external_cidr]
-  description       = "Allow workstation to communicate with the cluster API Server"
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.votingapp-cluster.id
-  to_port           = 443
-  type              = "ingress"
-}
-
-
-
-# Define security group for EKS cluster communication
-resource "aws_security_group" "votingapp-cluster" {
-  name        = "terraform-eks-votingapp-cluster"
-  description = "Cluster communication with worker nodes"
-  vpc_id      = aws_vpc.votingapp.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "terraform-eks-votingapp"
-  }
-}
-
-# Define security group rule for allowing workstation to communicate with the cluster API Server
-resource "aws_security_group_rule" "votingapp-cluster-ingress-workstation-https" {
-  cidr_blocks       = [local.workstation_external_cidr]
-  description       = "Allow workstation to communicate with the cluster API Server"
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.votingapp-cluster.id
-  to_port           = 443
-  type              = "ingress"
-}
+data "aws_availability_zones" "azs" {}
 
